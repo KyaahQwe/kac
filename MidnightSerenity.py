d4783f6c80cbeace74fa64d6177a896793483b6b
KyaahQwe <88075364+KyaahQwe@users.noreply.github.com>
2024-10-08 18:37:06 +0800

    Update MidnightSerenity.py

--- MidnightSerenity.py
+++ MidnightSerenity.py
@@ -5,7 +5,7 @@ st.header('wassup' , divider="violet")
 st.subheader('''
              Not sure how you've been doing recently but, i hope everything went well for you''')
 import time
-time.sleep(1)
+time.sleep(2)
 st.write("Though im not sure what to do")
 import time
 time.sleep(3)
@@ -17,40 +17,55 @@ import time
 time.sleep(2)
 st.write('Do you think you know what you are doing right now?')
 import time
-time.sleep(1)
-choice = st.radio("Choose One Darling", ["Yes,I Do", "No, I Dont"])
-if choice == 'Yes,I Do':
-    st.write('Then why not do it now right? Anyway a kitty pic for you')
+time.sleep(2)
+import streamlit as st
+
+agree = st.checkbox("Yes, I do", value=False)
+st.checkbox('No, I Dont', value=False)
+time.sleep(5)
+if agree:
+    st.write("Here's a picture of waguri for you to cheer u up")
 else:
-    st.write('Awww, i dont think ill be able to help much but here is some kitties picture for you')
-    time.sleep(2)
-st.image("https://i.pinimg.com/564x/ea/5b/30/ea5b303fbb727e71f12f76e2038cfdf3.jpg", caption="like i said it is kitties :3")
+ st.write('Awww, i dont think ill be able to help much but here is a photo of waguri for you')
+time.sleep(2)
+st.image("https://i.pinimg.com/736x/31/b0/40/31b0407e95440375c41befde2d467c6c.jpg", width=300,  caption="like i said it's waguri :3")
    
 time.sleep(4)
 st.write('So? Is it cute?')
-time.sleep(1)
-choice = st.radio('Choose carefully peasant...', ['IT IS SO CUTEEEE', 'nah its not that cute/ im a dog person'])
-if choice == 'IT IS SO CUTEEEE':
-    st.write('Just like you cutie🌷')
-else:
- st.write('how dare u nig-... ahem i mean peasant it is cute okay')
 time.sleep(2)
+import streamlit as st
+
+agree = st.checkbox("IT'S SO CUTEEEEE", value=False)
+st.checkbox('It is decent i guess', value=False)
+time.sleep(5)
+if agree:
+   st.write('IKR JUST LIKE YOU')
+   st.image('https://i.pinimg.com/736x/97/76/d5/9776d50f81b995d54668e31cad42239e.jpg', width=200)
+else:
+   st.write('how dare uuuuu')
+   st.image('https://i.pinimg.com/564x/a7/81/f7/a781f7e99ec58c8a793942ca135ab1a3.jpg', width=200)
+time.sleep(3)
+st.write('how dare u nig-... ahem i mean peasant it is cute okay')
+time.sleep(3)
 st.write('anyway back to our main topic here')
-time.sleep(2)
+time.sleep(3)
 st.write('so im gonna ask you some questions alright?')
-time.sleep(2)
-choice = st.radio('Do you have a problem in life?', ['Yes', 'No'])
-if choice == 'Yes':
-    st.write('Hmmm')
+time.sleep(3)
+st.write('Can do you anything about your problem?')
+agree = st.checkbox("Yes", value=False)
+st.checkbox('No', value=False)
+time.sleep(5)
+if agree:
+   st.write('Then why worry?')
+   st.image('https://preview.redd.it/waguri-has-brain-rot-v0-d4vu38roh4sd1.jpg?width=640&crop=smart&auto=webp&s=6bce7f8aaa810e02009f3f6b88bc63f5ecb1dca4', width=100)
 else:
-   st.write('I see....')
-genre = st.radio('Can you do anything about it', ['Yes', 'No'])
-if genre == 'Yes':
-    st.write('Then Why Worry')
-else:
-   st.write('Then Why Worry')
+   st.write('Then why worry?')
+   st.image('https://preview.redd.it/waguri-has-brain-rot-v0-ygv68u9oh4sd1.jpg?width=640&crop=smart&auto=webp&s=3ed9a02e8f18af5e001238f961c1a3b3435c55ce', width=100)
+time.sleep(3)
+st.write('As much as i hate to admit this but im bad at comforting people so this is the best i could do, sorry')
 time.sleep(2)
-st.write('Anyway thank you for answering even though its quite short')
-time.sleep(1)
-st.write('Im thankful')
-st.image("https://i.pinimg.com/564x/fc/97/34/fc973460640f23260b51dc8f037547a7.jpg", caption="Love U <3")
+st.image('https://steamuserimages-a.akamaihd.net/ugc/948471427338598117/33681F4EEA1FD6ABF6E11A67CB134973586EC08E/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false', width=200)
+time.sleep(2)
+st.write('I hope you enjoyed it though')
+time.sleep(2)
+st.image("https://i.pinimg.com/736x/d9/75/71/d97571ce461c5e97be3d4eb3475226cb.jpg", width=300, caption="Love U RAHHHHH <3")
